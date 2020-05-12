5ca0d663301f47c5ee5091ca0953ac4d966724a9
steroptes <rjeb.sfar.med@gmail.com>
2020-05-12 22:59:07 +0100

    ES6 checkpoint first modification

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
-var pets = [
+let pets = [
   { name: "Max", type: "dog", bornOn: 2018 },
   { name: "Angel", type: "cat", bornOn: 2015 },
   { name: "Jasper", type: "dog", bornOn: 2016 }
 ];
 
-function getAge(pet) {
-  return new Date().getFullYear() - pet.bornOn;
-}
-
-var petsWithAge = [];
-for (var i = 0; i < pets.length; i++) {
-  var pet = pets[i];
-  pet.age = getAge(pet);
-
-  petsWithAge.push(pet);
-}
-console.log(petsWithAge);
-
-var dogs = [];
-for (var i = 0; i < pets.length; i++) {
-  var pet = pets[i];
-  if (pet.type === "dog") {
-    dogs.push(pet);
-  }
-}
-console.log(dogs);
-
-var jasper;
-for (var i = 0; i < pets.length; i++) {
-  var pet = pets[i];
-  if (pet.name === "Jasper") {
-    jasper = pet;
-  }
-}
-console.log("Jasper is " + jasper.age + " years old");
+getAge=(pet) => new Date().getFullYear() - pet.bornOn;
+pets.map(pet => {pet.age = getAge(pet)});
+console.log(pets);
+let jasper = pets
+  .filter(pet => pet.type === "dog")
+  .find(dog => dog.name === "Jasper");
+console.log(`Jasper is ${jasper.age} years old`);
